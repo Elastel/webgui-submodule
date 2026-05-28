790735304b0e8a1315886db4a8ab441247a9990e
Elastel <110442734+Elastel@users.noreply.github.com>
2026-05-28 12:12:56 +0800

    Update condition for displaying LTE Network Status

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -129,7 +129,7 @@
                     </br>
                   <?php } ?>
                   <?php endforeach ?>
-                  <?php if (isset($lteInfo['iccid']) && strlen($lteInfo['iccid']) > 0) { ?>
+                  <?php if (isset($lteInfo['iccid']) && strlen($lteInfo['iccid']) > 2) { ?>
                     <h4 class="card-title"><?php echo _("LTE Network Status"); ?></h4>
                     <div class="row ml-1">
                       <div class="col-sm">
